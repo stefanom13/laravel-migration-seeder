9602d3bc039fe0f1460a964c2d6e415167874d8f
stefanom13 <stefanomancuso13@gmail.com>
2022-04-28 16:31:24 +0200

    layout completato esercizio finito

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>laravel-migration-seeders</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
-   @foreach($trains as $train)
-   <div>
-    {{-- {{$train['azienda']}} <br> --}}
-  </div>
-   @endforeach
+    <h1>Trains</h1>
+    <div class="container">
+        <ul>
+            @foreach ($trains as $train)
+                <li class="trains">
+                    <div>
+                        <h5>Azienda</h5>
+                        {{ $train['azienda'] }} <br>
+                    </div>
+                    <div>
+                        <span class="codT">cod.Treno</span>
+                        {{ $train['codice_treno'] }}
+                    </div>
+                    <div>
+                        <p>partenza</p>
+                        {{ $train['stazione_di_partenza'] }}
+                    </div>
+                    <div>
+                        <p>arrivo</p>
+                        {{ $train['stazione_di_arrivo'] }}
+                    </div>
+                    <div>
+                        <span class="codT">orario arrivo</span>
+                        {{ $train['orario_di_arrivo'] }}
+                    </div>
+                    <div>
+                        <span class="codT">orario partenze</span>
+                        {{ $train['orario_di_partenza'] }}
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </body>
+
 </html>
